c96a08afeb2a008f8e6c28e70f5778f743383969
Rajnish Pandey <piyushwinner1997@gmail.com>
2023-08-01 12:44:38 +0530

    Alternative way of conditional rendering

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,6 +22,16 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(context) {
+    // final screenWidget = activeScreen == 'start-screen'
+    //           ? StartScreen(switchScreen)
+    //           : const QuestionsScreen();
+
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if(activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -35,9 +45,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
