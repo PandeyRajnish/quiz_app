37973dd689f64598004a09919e868d8d89dea7a2
Rajnish Pandey <piyushwinner1997@gmail.com>
2023-08-08 09:27:50 +0530

    replaces with previous code

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,10 +4,10 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
-  List<String> get shuffledAnswers {
+  List<String> getShuffledAnswers() {
     final shuffledList =
-        List.of(answers); 
-    shuffledList.shuffle();
+        List.of(answers); // creates the copy of the answer list
+    shuffledList.shuffle(); // changes the list place
     return shuffledList;
   }
 }
